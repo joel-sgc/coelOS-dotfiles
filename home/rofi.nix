1aa480dd4c6df272411db3ea2d35e4b9c4a4913b
joel-sgc <joeloultook@gmail.com>
2026-08-12 00:29:33 -0400

    Add Logout to the rofi power/system menu
    hyprctl dispatch exit -- same mechanism as home/hyprland.nix's
    existing $mainMod+M keybind, just exposed in the menu too. Icon is
    nf-md-logout (U+F0343), matching the Material Design Icons family
    already used for the other entries in this menu.

--- home/rofi.nix
+++ home/rofi.nix
@@ -191,6 +191,7 @@ let
     #!/usr/bin/env bash
     choice=$(printf \
     "  Lock\n\
+󰍃  Logout\n\
 󰤄  Suspend\n\
   Restart\n\
 󰤆  Shutdown\n" | rofi -dmenu -i -p "Power" -lines 10 -no-fixed-num-lines)
@@ -199,6 +200,7 @@ let
 
     case "$choice" in
     	*Lock*) hyprlock ;;
+    	*Logout*) hyprctl dispatch exit ;;
     	*Suspend*) systemctl suspend ;;
     	*Restart*) systemctl reboot ;;
     	*Shutdown*) systemctl poweroff ;;
